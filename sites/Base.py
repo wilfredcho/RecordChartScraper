33856408d7be38995dec4dc5306ccf5bda33ef6b
Wilfred Cho <wilfredcho@gmail.com>
2018-12-23 23:21:00 -0800

    add comment

--- sites/Base.py
+++ sites/Base.py
@@ -7,6 +7,8 @@ class Base(ABC):
 
     @abc.abstractmethod
     def proc_row(self, row, chart):
+    #return proc_info(chart, cur_pos, last_pos, title, artist)
 
     @abc.abstractmethod
     def run(self, soup, chart):
+    #return [self.proc_row(row, chart) for row in rows if bool(self.proc_row(row, chart))]
