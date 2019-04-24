300637f31a110f08e32873cce5622c0faac133a9
Wilfred Cho <wilfredcho@gmail.com>
2019-04-23 20:12:13 -0700

    fix NL new songs

--- sites/NL.py
+++ sites/NL.py
@@ -6,11 +6,12 @@ class NL(Base):
     def proc_row(self, row, chart):
         cur_pos = format_text(row.find_all("td", {"class": "text"})[0].text)
         last_pos = format_text(row.find_all("td")[1].text)
-        if last_pos:
-            artist_title = row.find("a", {"class": "navb"}).text
-            artist = row.find("a", {"class": "navb"}).select("b")[0].text
-            title = artist_title.replace(artist, "")
-            return proc_info(chart, cur_pos, last_pos, title, artist)
+        if last_pos is None:
+            last_pos = 101
+        artist_title = row.find("a", {"class": "navb"}).text
+        artist = row.find("a", {"class": "navb"}).select("b")[0].text
+        title = artist_title.replace(artist, "")
+        return proc_info(chart, cur_pos, last_pos, title, artist)
 
     def run(self, soup, chart):
         table = soup.find("table")
